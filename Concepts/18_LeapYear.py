e615f3b347366679f5b2f90802a9a23e915907fb
Sakshi <iamsakshiingole@gmail.com>
2023-08-13 21:36:52 -0400

    Files added

---
+++ Concepts/18_LeapYear.py
@@ -0,0 +1,7 @@
+# Input year and check whether it is leap or not.
+
+n=int(input("Enter year: "))
+if n%4==0:
+    print("Is is a leap year.")
+else:
+    print("It is not a leap year.")
---
+++ Concepts/19_PrintPrimeNos.py
@@ -0,0 +1,10 @@
+# Print prime numbers in 1 to 100. 
+
+print("Prime numbers between 1 and 100 are: ")
+for n in range (1, 101):
+    if n>1:
+        for i in range(2,n):
+            if (n%i)==0:
+                break
+        else:
+            print(n, end=" ")
