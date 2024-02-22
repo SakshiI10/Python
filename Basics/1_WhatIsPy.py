3dbbf5c42b34d8a243903ef7a002b664ecec68e4
Sakshi <iamsakshiingole@gmail.com>
2024-02-21 22:27:40 -0500

    Files added and Modified

---
+++ Basics/1_WhatIsPy.py
@@ -0,0 +1,39 @@
+''' 
+Introduction to Python:
+1. Python is an interpreted, high level, general purpose, object oriented, versatile and very popular open source programming   language.
+
+2. It is designed by Guido Van Rossum in 1991.
+
+3. It is named after Monty Python.
+
+4. Python can be used for any purpose like web development(server side), system scripting, scientific application, software development, etc.
+
+5. Python is ideally designed for rapid prototyping of complex applications and is widely used in advanced fields of computing like Artificial Intelligence, Natural Language Generation, Neural Networks, etc.
+
+6. It is also used in connecting database system.
+
+7. It can work on different platforms like Windows, Mac, Linux, Raspberry Pie, etc.
+
+8. It can be treated as a functional way or procedural way or an object oriented way.
+
+9. A development Environment called IDLE (Integrated Development Environment) has been bundled with the default implementation of the language.
+
+10. Features of Python:
+    i) Variety of Basic Data Types.
+    ii) Supports OOP
+    iii) Has Modules and Packages
+    iv) Supports Exceptional Handling
+    v) Has strong and dynamically typed datatype
+    vi) Contains advanced programming features like generators and list comprehensions.
+    vii) Supports Automatic Memory Management.
+    
+11. Python uses Interpreter (It converts high level code into machine language line by line).
+
+12. Interpreter works as: Source Code -->  Interpreter  -->  Output
+
+13. Interpreter can be used in 2 ways:
+    i) Interactive mode
+    ii) Script mode
+    
+14. Python Source file are treated as encoded in UTF-8.
+'''
