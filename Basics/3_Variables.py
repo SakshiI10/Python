acdcffe755391b966a8018f541b0ce7b349b817a
Sakshi <iamsakshiingole@gmail.com>
2024-02-21 22:59:16 -0500

    Files added and Modified

---
+++ Basics/3_Variables.py
@@ -0,0 +1,18 @@
+''' 
+Python Variables:
+1. Python is completely Object Oriented hence we do not need to declare variable name or thier type before using them.
+
+2. Every variable in Python ia an Object.
+
+3. An assignment statements creates a new ariable and assigns value.
+
+4. Variable names are case sensitive.
+
+5. Variable name may consists of alphabets, digits and underscore.
+
+6. Python doesn;t support declaring variables. They are just containers for storing values.
+
+7. Python has 2 basic types of variables:
+    i) Numbers
+    ii) Strings
+'''
