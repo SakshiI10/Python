9514209f760fc7b9bfbe1ea7ca74baa3e4863e21
Sakshi <iamsakshiingole@gmail.com>
2024-02-22 22:06:02 -0500

    Files added and Modified

---
+++ Basics/8_EscapeSequence.py
@@ -0,0 +1,21 @@
+''' 
+Escape Sequence in Python:
+1) \ is a special character caled escape sequence character.
+2) Python made following escape sequence characters available for output formatting
+3)  Sr No       Escape Sequence      Use/Purpose
+    1)          \                    New line
+    2)          \\                   Prints single backslash
+    3)          \'                   Prints single quote
+    4)          \''                  Prints double quote
+    5)          \a                   ASCII audible Bell
+    6)          \b                   ASCII Backspace
+    7)          \f                   ASCII form feed
+    8)          \n                   ASCII line feed
+    9)          \r                   ASCII carriage return
+    10)         \t                   ASCII horizontal tab
+    11)         \v                   ASCII vertical tab
+    12)         \ooo                 Character with octal value 000
+    13)         \xhh                 Character with hexadecimal value hh
+    14)         \uxxxx               Character with 16-bit value xxxx
+    15)         \uxxxxxxxx           Character with 32-bit value xxxxxxxx
+ '''
---
+++ Basics/9_EscapeSequence2.py
@@ -0,0 +1,11 @@
+print("one \
+    two \
+    three")
+print("\\ is single slash")
+print("\' is single quote")
+print("\" is double quote")
+print("Line1\nLine2...")
+print("Line1\rLine2")
+print("One\tTwo")
+print("\101\102\103")
+print("\x6e\x6f")
