577efb4fb280e284260d87bc5101cf5b486e5997
Sakshi <iamsakshiingole@gmail.com>
2024-02-22 21:41:51 -0500

    Files added and Modified

---
+++ Basics/7_Keywords.py
@@ -0,0 +1,39 @@
+''' 
+Keywords:
+1) Python has reserved some words that programmed cannot use as any identifier name of variable oe function.
+2) Such reserved words are called key words of language.
+3)  Sr No.  Keyword     Meaning/Purpose
+    1)      and         A logical operator
+    2)      as          To create an alias
+    3)      assert      For debugging
+    4)      break       To break execution sequence and transfer control out of loop
+    5)      class       To define a class
+    6)      continue    To continue to the next iteration of loop
+    7)      def         To define new function
+    8)      del         To delete an object
+    9)      elif        Used in if statement to indicate else if
+    10)     else        Used in if statement to indicate else
+    11)     except      Specifies what to do when an exception occurs
+    12)     False       Boolean Value
+    13)     finally     Used in exception handling
+    14)     for         Used in lopp
+    15)     from        To import specific part of module
+    16)     global      To declare global variable
+    17)     if          To define conditional statement
+    18)     import      To import a module
+    19)     in          Check if value present in list, tuple, etc
+    20)     is          To test if two variable are equal
+    21)     lambda      To create an anonymous funtion
+    22)     None        Null value
+    23)     Nonlocal    To create non-local variable
+    24)     not         Logical operator
+    25)     or          Logical operator
+    26)     pass        A null statement that does nothing
+    27)     raise       To raise an exception
+    28)     return      Returns control and value to calling routine
+    29)     True        Boolean value
+    30)     try         To make try
+    31)     while       To create a while loop
+    32)     with        To simplify exceptional handling
+    33)     yeild       To end a funtion, return generator
+'''
