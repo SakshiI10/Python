75d40ecfdc372fc9d5debb71794d6f6966eb4083
Sakshi <iamsakshiingole@gmail.com>
2024-02-22 21:15:29 -0500

    Files added and Modified

---
+++ Basics/5_DataTypes.py
@@ -0,0 +1,13 @@
+''' 
+Datatype:
+Defines the type of variable, these are actually classes and variables are instance of those classes.
+
+Sr No   Text Type              
+1       Numeric type        int, float, complex
+2       Sequence type       list, tuple, range
+3       Mapping type        dict
+4       Set type            set, frozenset
+5       Boolean type        bool
+6       Binary type         bytes, bytearray, memory view
+7       None type           none
+'''
