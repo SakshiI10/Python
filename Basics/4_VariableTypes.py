613112c76def0917bb7341f77053ccc8b1bf65ea
Sakshi <iamsakshiingole@gmail.com>
2024-02-22 21:01:08 -0500

    Files added and Modified

---
+++ Basics/4_VariableTypes.py
@@ -0,0 +1,37 @@
+''' 
+1) Numbers: 
+   Python supports 3 types of numbers: 
+      i) Integers or fixed point numbers
+      ii) Reals or FLoating point numbers
+      iii) Coplex numbers
+'''
+a=10
+b=20
+c=a+b
+print(c)
+
+price=25.5
+qty=7
+amount=price*qty
+print(amount)
+
+c1=2+3j
+c2=5-2j
+c3=c2+c1
+print(c3)
+print(c1*c2)
+
+''' 
+2) Strings:
+   i) String literal is a value that consists of alphabets, digits and special symbols.
+   ii) String can be enclosed in single quotes('..') or double quotes("...")
+   iii)Backslash(\) can be used to escape quotes and interprete following charater as a special character.
+   1v) Use (+) to conncatenate string variables and/or literals.
+'''
+print('I said,\'All is well\'')
+
+good='Good'
+day='Day'
+print(good+" "+day)
+
+print('I said,\'All is well\'')
