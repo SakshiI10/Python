4d046bc24db190b0e4fcfcdd561143652d839e3b
Sakshi <iamsakshiingole@gmail.com>
2024-02-23 22:45:57 -0500

    File modified

---
+++ Basics/12_PyStatements.py
@@ -0,0 +1,41 @@
+''' 
+Python Statements are broadly divided into 2 categories
+1) Simple Statement
+i) Expression
+
+ii) Assignment
+
+iii) assert: This asserts or states the fact confidentially in the program, these are boolean expressions that are evaluated as True or False.
+
+iv) pass: It is used when it is syntactically required to show 'do nothing'.
+
+v) del: It is used to delete variable, user defined objects, lists, items within list, dictionaries, etc.
+
+vi) return: It is usedto return control and/or value to calling function.
+
+vii) yeild: Yeild can be used instead of return. The difference is that, return statement terminates a function entirely whereas yield pauses the function savng all its states and later continues from there on successive function call.
+
+viii) raise
+
+ix) break: It is used only in for or while loop, it terminates the loop and transfers control out of the loop.
+
+x) continue: It is used in for or while loop, it continues the execution with the next cycle of the nearest closing loop,
+
+xi) import: It is used to import the module and access the definitions inside imported module. It has 2 clauses, as and from. as: the name following as is bound directly to the imported module and from: is used to import specific attributers and functions only.
+
+xii) global: This is a declaration statement which holds the value for the entire current code block.
+
+xiii) future: This is declarative to the compiler to compile particular module using syntax that will be available in specified future release of Python.
+
+xiii) non local: It is the category of variables that are used in nested function whose local scope is not defined.
+
+
+2) Compound Statement
+i) if: It evaluates given test conditions.
+
+ii) while: This is an iterative statement where a block of code executes repeatedly as along as the test condition is true.
+
+iii) for: It allows to perform some action for each item in a list.
+
+# DIfference between for and while loop is that, for loop iterates over a sequence whereas while loop iterates against a predefined condition.
+# '''
