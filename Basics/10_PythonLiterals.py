0ebea32704a3b8e72d792c4542cb3f0bb79df5a1
Sakshi <iamsakshiingole@gmail.com>
2024-02-23 21:54:16 -0500

    File modified

--- Basics/10_PythonLiterals.py
+++ Basics/10_PythonLiterals.py
@@ -7,6 +7,28 @@ Literals are notations for constant values of some in-built data types.
     There are no complex literals in Python.
     Numeric Literals don't include sign.
 '''
-''' 
-i)Numeric Literlas:
-'''
+
+#i) Integer Literals:
+print(12_345)
+print(0x123)
+print(0o50)
+print(0b1011001)
+
+#ii) Floating Point Literals
+print(123.456)
+print(12_34.45_67)
+print(0.01)
+print(0.001)
+print(0.0001)
+print(0.00001)
+print(0.000001)
+print(1.23e4)
+print(0e0)
+print(77e05)
+
+#iii) Imaginery Literals
+print(1.23j)
+print(5j)
+print(0.02j)
+print(.01j)
+print(1.23e3j)
