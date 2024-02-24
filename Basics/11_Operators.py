b100ff150873c703f8456de119b36f7ea6bbaa0a
Sakshi <iamsakshiingole@gmail.com>
2024-02-23 22:18:09 -0500

    File modified

---
+++ Basics/11_Operators.py
@@ -0,0 +1,74 @@
+''' 
+Operators:
+1) Operations are performed using operators on variables and values.
+2) Python divides operators in following groups:
+    i) Arithmatic Operators
+    ii) Assignment Operators
+    iii) Comparison Operators
+    iv) Logical Operators
+    v) Identity Operators
+    vi) Membership Operators
+    vii) Bitwise Operators
+'''
+
+''' 
+i) Arithmatic Operators:
+Operator    Name                Exampple (a=11, b=3)
++           Addition            a+b=14
+-           Subtraction         a-b=8
+*           Multiplication      a*b=33
+/           Division            a/b=3.666666666
+%           Modulus             ab=2
+**          Exponentiation      a**b=1331
+//          Floor division      a//b=3
+
+ii) Assignment Operators
+Operator    Name                            Example (a=15, b=4)
+=           Assign                          a=15, b=4
++=          Add and Assign                  a+=b i.e., a=19
+-=          Subtract and Assign             a-=b i.e., a=11
+*=          Multiply and Assign             a*=b i.e., a=60
+/=          Divide and Assign               a/=b i.e., a=3.75
+5=          Modulus and Assign              a%=b i.e., a=3
+//=         Floor division and assign       a//=b i.e., a=3
+**=         Exponentiation and assign       a**=b i.e., a=50625
+&=          Bitwise AND and assign          a&=b i.e., a=4
+|=          Bitwise OR and assing           a|=b i.e., a=15
+^=          Bitwise XOR and assign          a^=b i.e., a=11
+>>=         Right shift and assign          a>>=b i.e., a=0
+<<==        Left shift and assign           a<<=b i.e., a=240
+
+iii) Comparison Operator
+Operator    Name                            Example
+==          Equal                           a==b ==> False
+!=          Not Equal                       a!=b ==> True
+>           Greater than                    a>b ==> False
+<           Less than                       a<b ==> True
+>=          Greater than or equal to        a<=b ==> False
+<=          Less than or equal to           a<=b ==> True
+
+iv) Logical Operator
+Operator    Name                            Example (a=4, b=11)
+and         Logical conjunction             a<5 and b<5 => False 
+or          Logical disjunstion             a<5 and b<5 => True
+not         Logical negation                not a<5 => False
+
+v) Identity Operators
+Operator    Name
+is          Logical
+is not      Not Logical
+
+vi) Membership Operators
+Operator    Name
+in          Member
+not in      Not member
+
+vii) Bitwise Operator
+Operator    Name
+&           AND
+|           OR
+^           XOR
+~           NOT
+<<          Left Shift zero fill
+>>          Right shift zero fill
+'''
