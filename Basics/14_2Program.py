7439870bfcb698b16ee5b1a58c62c402130165e9
Sakshi <iamsakshiingole@gmail.com>
2024-02-24 22:03:53 -0500

    File added

---
+++ Basics/14_2Program.py
@@ -0,0 +1,7 @@
+#Input 2 numbers and find smaller and larger.
+n=int(input("Enter a number: "))
+m=int(input("Enter a number: "))
+if n<m:
+    print(n,"is smaller")
+else:
+    print(m,"is smaller")
