983be83f7d5eeaa71e419536d0b5ecd2e342b4c3
Sakshi <iamsakshiingole@gmail.com>
2024-02-24 22:17:41 -0500

    File added

--- Basics/15_3Program.py
+++ Basics/15_3Program.py
@@ -1,2 +1,12 @@
 #Input a number and coputer its factorial.
 
+n=int(input("Enter a number: "))
+f=1
+if n<0:
+    print("Factorial of negative number doesn't exist")
+elif (n==0):
+    print("Factorial of 0 is 1")
+else:
+    for i in range(1,n+1):
+        f=f*i
+    print("The factorial is:",f)
---
+++ Basics/16_4Program.py
@@ -0,0 +1,7 @@
+#Print numbers 1...50 and calculate their sum.
+
+sum=0
+for i in range(1, 51):
+    print(i)
+    sum=sum+i
+print(sum)
