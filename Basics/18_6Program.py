7283e64c4c57d6b1212c36a75a022864a9dd6132
Sakshi <iamsakshiingole@gmail.com>
2024-02-25 21:58:59 -0500

    File added

--- Basics/18_6Program.py
+++ Basics/18_6Program.py
@@ -2,12 +2,12 @@
 n=int(input("Enter a number: "))
 n1=0
 n2=1
-print(n1)
-print(n2)
+print(n1, end=" ")
+print(n2, end=" ")
 count=3
 for i in range(count, n+1):
     n3=n1+n2
-    print(n3)
+    print(n3, end=" ")
     n1=n2
     n2=n3
     count+=1
--- Basics/19_7Program.py
+++ Basics/19_7Program.py
@@ -1,1 +1,6 @@
-#
+# Print alphabet a...z
+
+ch=97
+while ch<=122:
+    print(chr(ch),end=" ")
+    ch=ch+1
