78d872e1ea77cc2a0a1320b4fe89daac3e9fb869
Sakshi <iamsakshiingole@gmail.com>
2024-02-25 22:09:51 -0500

    File added

--- Basics/22_10Program.py
+++ Basics/22_10Program.py
@@ -1,3 +1,13 @@
 # Print prime factors of a number.
 
-n=int(input("Enter a num"))
+n=int(input("Enter a number: "))
+print("Prime factors of",n,"are")
+i=2
+while i<=n:
+    if n%i==0:
+        print(i, end=" ")
+        n=n/i
+        continue
+    i=i+1
+if n>1:
+    print(int(n))
