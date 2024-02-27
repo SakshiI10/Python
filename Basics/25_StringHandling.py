c072271897cb0109c2ffdb444bd0563dc39f7d6b
Sakshi <iamsakshiingole@gmail.com>
2024-02-26 22:34:08 -0500

    File added

---
+++ Basics/25_StringHandling.py
@@ -0,0 +1,38 @@
+''' 
+String Handling:
+1) Python does not have character datatype as in Python character is also considered as string of length one.
+
+2) Like array C in C++, Python handles characters of string using square bracket and subscript i.e. index no i.e. position.
+
+3) Position of first character in string is always zero.
+
+4) We can extract characters from string if their position is known.
+
+
+String Operators:
+Sr.No.      Operators       Purpose
+1           +               concatenation
+2           *               repeats
+3           []              slice
+4           [:]             range slice
+5           in              membership
+6           not in          inverse of in
+7           r               suppresses escape sequence
+8           %               string formatting
+% symbol in string formatting is the prefix to another character that defines the type of value to be inserted.
+
+Sr No.      Character       Purpose
+1           %c              Character
+2           %s              String
+3           %i              Signed decimal integer
+4           %d              Signed decimal integer
+5           %u              Unsigned decimal integer
+6           %o              Octal number
+7           %x              Hexadecimal integers with lowercase letters
+8           %X              Hexadecimal integers with uppercase letters
+9           %e              Exponential notation with lowercase e
+10          %E              Exponential notation with lowercase E
+11          %f              Floating point real number
+12          %g              The shorter of %f and %e
+13          %G              The shorter of %f and %E
+'''
