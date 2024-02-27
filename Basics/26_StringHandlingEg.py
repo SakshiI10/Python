753fccefea2b06c8813108b700522468657ac618
Sakshi <iamsakshiingole@gmail.com>
2024-02-26 22:42:58 -0500

    File added

---
+++ Basics/26_StringHandlingEg.py
@@ -0,0 +1,33 @@
+''' print('Welcome to Python')
+print("Welcome to Python")
+print(''Welcome to Python'')
+
+str1="your's faithfully"
+print(str1)
+
+str2='she is a "very good" student'
+print(str2)
+
+s1="Welcome"
+s2='to'
+s3=''Python''
+print(s1 + s2 + s3)
+
+print("Good Morning\nWel come to Python")
+ '''
+city='Nagpur'
+print(city)
+print(city[0])
+print(city[1])
+print(city[5])
+print(city[1:3])
+print(city[1:6])
+print(city[0:6])
+print(city[2:])
+print(city[:4])
+print(len(city))
+print(city[len(city)-3:len(city)])
+print(city[-1])
+print(city[-3:-1])
+print(city[-3:0])
+print(city[-3:])
