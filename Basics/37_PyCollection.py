6add656681de71b9d6b5da04d7e9d183b7a1c8ff
Sakshi <iamsakshiingole@gmail.com>
2024-03-01 15:06:45 -0500

    Python Collection

---
+++ Basics/37_PyCollection.py
@@ -0,0 +1,14 @@
+''' 
+1) Python Programming language supports collection of five data types.
+a) List: It is a collection which is ordered and mutable and it allows duplicate members.
+
+b) Array: It is a data structure that scores values of same data type.
+
+c) Tuple: It is a collection which is ordered and immutable and it allows duplicate members.
+
+d) Set: It is a collection which is unordered and unindexed and it does not allow duplicate members.
+
+e) Dictionary: It is a collection which is unordered, mutable and indexed and it does not allow duplicate members.
+
+2) Efficiency of the program depends on the selection appropriate collection type.
+'''
