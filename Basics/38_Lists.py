a4c68f2a0dc3c0710f9758e30c7daec3af536aaf
Sakshi <iamsakshiingole@gmail.com>
2024-03-01 15:38:40 -0500

    List

---
+++ Basics/38_Lists.py
@@ -0,0 +1,44 @@
+''' 
+Lists: 
+1) List is a collection of members which are ordered and changeable.
+
+2) List are written with square brackets.
+
+3) List members are separated by comma.
+
+4) Python allows nested lists.
+
+5) List members are called elements or items.
+
+6) List members are accesses using index.
+
+7) Index is always an integer and starts with zero.
+
+8) Python also allows to access members in reverse using negative indexing.
+
+9) Index of last element is -1, that of second last of -2, that of third last is -3 and so on.
+'''
+
+# a) Creating empty list
+a=list()
+print(a)
+
+# b) Writing lists
+num = [1,2,3,4,5,6,7,8,9,0]
+print(num)
+
+mix=['zero', 1, 'two', 3.0, 4, 'five', 6.0]
+print(mix)
+print(mix[0])
+print(mix[1])
+print(mix[2])
+print(mix[3])
+print(mix[4])
+print(mix[5])
+print(mix[6])
+print(mix[-1])
+print(mix[-2])
+print(mix[-3])
+print(mix[-4])
+print(mix[-5])
+print(mix[-6])
