58237f5e3751526d136fe3d72e06806d5fc1ad82
Sakshi <iamsakshiingole@gmail.com>
2024-03-01 15:44:17 -0500

    List As an Array

---
+++ Basics/39_ListAsArray.py
@@ -0,0 +1,14 @@
+''' 
+List as an Array:
+1) Although Python provides Array, but we can represent list as an Array.
+
+2) In array, Horizontal Entries are rows and Vertical entries are column.
+
+3) 2-D Array is represented using nested list i.e. List within List, where each element is treated as row of matrix.
+'''
+
+x=[[1,2,3,4],[5,6,7,8]]
+print(x)
+print(len(x))
+print(len(x[0]))
+print(x[0][1])
