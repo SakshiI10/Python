e138c4d4e25c36b1212bb369a60527f395beb554
Sakshi <iamsakshiingole@gmail.com>
2024-04-29 17:12:50 -0400

    File added

---
+++ Basics/49_IndexingSlicing.py
@@ -0,0 +1,13 @@
+# 1) Indexing:
+tup=(10, 20, 30, "python", 40, 50, 60)
+
+print(tup[:])
+print(tup[0:3])
+print(tup[0:-1])
+print(tup[-3:-1])
+
+# 2) Slicing: 
+print(tup[:])
+print(tup[0:3])
+print(tup[0:-1])
+print(tup[-3:-1])
---
+++ Basics/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+print(tup[0:3])
+print(tup[0:-1])
+print(tup[-3:-1])
