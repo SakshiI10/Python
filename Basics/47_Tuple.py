a25efa83d5a7a0a2231011999e85841494287957
Sakshi <iamsakshiingole@gmail.com>
2024-04-29 16:52:28 -0400

    File added

---
+++ Basics/47_Tuple.py
@@ -0,0 +1,10 @@
+tup1 = (24)
+print(type(tup1))
+
+tup2 = ("Hello")
+print(type(tup2))
+
+tup3 = (24, )
+print(type(tup3))
+
+#Hence, more than one object is required in tuple
---
+++ Basics/48_MultipleEleTuple.py
@@ -0,0 +1,26 @@
+''' 
+Multiple Elements in a Tuple:
+1) Tuple can have integers only.
+Tup = (10, 20, 30, 40, 50)
+
+2) Tuple can have integers, string, float values as well.
+Tup = (33, "Hello", 12.5, -122, "python)
+
+3) Tuple of list
+Tup = ([1, 2], [3, 4], [5, 6])
+
+4) Tuple allows duplicate values too
+Tup = ("hello", 100, [1, 2, 3], "world", "hello")
+
+5) Nested Tuple
+Tup = ((1, 2, 3), ('a', 'b', 'c'))
+
+6) Tuple can be declared without parentheses
+Tup = 10, 20, -50, "python"
+
+7) Empty tuple can be define.
+Tup = ()
+
+8) With Tuple() Constructor
+Tup=tuple(10, 20, 30, 40, 50)
+'''
