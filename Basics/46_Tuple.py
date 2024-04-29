a5c8e746d36f2b3fd3f9a23e182ee91aeeda95fd
Sakshi <iamsakshiingole@gmail.com>
2024-04-29 16:40:51 -0400

    File added

---
+++ Basics/46_Tuple.py
@@ -0,0 +1,19 @@
+''' 
+Tuple:
+1) Tuple is a collection of objects seperated by commas and enclosed using ().
+
+2) Tuple is one of the 4 built in data types in Python.
+
+3) Tuples are heterogenous sequence of elements.
+
+4) Tuples are immutable (unchangeable).
+
+5) Tuple is similar to a list in terms of indexing, nested objects and repetition but a tuple is immutable, unlike lists which are mutable.
+
+6) It occupies less memory compare to list.
+
+7) Allows duplicates, since tuples are indexed, they can have with the same value.
+
+8) We can create tuple by writing elements seperated by commas inside parentheses.
+Tup=(10, 20, 30, 40, 50);
+'''
