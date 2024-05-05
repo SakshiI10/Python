b0c9581c8661d472517cc44f03bf5e7dc6a64b03
Sakshi <iamsakshiingole@gmail.com>
2024-05-05 17:44:54 -0400

    File added

--- Basics/50_UpdatingTuple.py
+++ Basics/50_UpdatingTuple.py
@@ -1,3 +1,24 @@
 ''' 
-1) We can change the tuple into a list
+We can change the tuple into a list, change the list, and convert the list back into a tuple.
 '''
+
+# Change Tuple Values
+x = ('cat', 'dog', 'cow')
+y=list(x)
+y[1] = 'chicken'
+x=tuple(y)
+print(x)
+
+# Add items
+y.append('goat')
+x=tuple(y)
+print(y)
+
+# Add tuple to tuple
+z=('zebra',)    #, is must
+x=x+z
+print(x)
+
+''' 
+Remove Items: We can't remove items from tuple.
+''' 
