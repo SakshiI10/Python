58fdac501a214a15e46a4e64c09d2cfe5387d197
Sakshi <iamsakshiingole@gmail.com>
2024-05-13 14:37:14 -0400

    File added

---
+++ Basics/54_Set.py
@@ -0,0 +1,23 @@
+''' 
+Sets: 
+1. Set are used to store multiple items in a single variable.
+
+2. Sets are written with curly brackets.
+
+3. set={"apple", "banana", "cherry"}
+
+4. Set is an unordered collection of data types(elements) that is iterable.
+
+5. The items in a set don't have a defined order.
+
+6. Set is mutable so we can modify it. Once a set is created, you can't change its items, but you can remove items and add new items.
+
+7. No duplicate elemenets are allowed.
+
+8. Sets can also be used to perform mathematical set operation like union, intersection, symmentric difference, etc.
+
+9. Set elements can be of different types except mutable element like lists, set or dicctionary.
+set1={1,2.0,(3,4,5,6),"python"}
+'''
+set1={1,2.0,(3,4,5,6),"python"}
+print(set1)
