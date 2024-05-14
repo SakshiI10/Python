46a0052f4eab5f9c4aff27f26ae2e77ff7713930
Sakshi <iamsakshiingole@gmail.com>
2024-05-13 22:09:08 -0400

    File added

--- Basics/61_2Program.py
+++ Basics/61_2Program.py
@@ -1,1 +1,20 @@
-# Add elements in a set.a
+# Add and remove elements in a set.
+setF=set()
+print(setF)
+
+setF.add("Banana")
+print(setF)
+
+setF.update(["Apple", "Chickoo"])
+print(setF)
+
+numSet=([1,2,3,4,5])
+print("Origignal set: ", numSet)
+#numSet.pop()
+#print("After pop(): ", numSet)
+
+numSet.discard(3)
+print("After discard: ", numSet)
+
+numSet.remove(5)
+print(numSet)
---
+++ Basics/62_3Program.py
@@ -0,0 +1,14 @@
+# Find the length of a set.
+
+set={5, 10, 15, 20, 25, 30}
+print(set)
+print(type(set))
+print(len(set))
+
+set2={1,1,1,1,1}
+print(set2)
+print(type(set2))
+
+set3={1,1,1,1,1,2,2}
+print(set3)
+print(type(set3))
