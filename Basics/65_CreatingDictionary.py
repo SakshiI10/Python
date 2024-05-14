e645b3dc275a3e4b4881077355a1153fb153d810
Sakshi <iamsakshiingole@gmail.com>
2024-05-14 13:24:43 -0400

    File added

---
+++ Basics/65_CreatingDictionary.py
@@ -0,0 +1,27 @@
+''' 
+Creating Dictionary:
+Dictionary keys are case sensitive, the same name but different cases of key will be treated distinctly.
+'''
+d1={'a': 1, 'b': 2, 'c':3}
+d2={'A': 1, 'B': 2, 'C':3}
+print(d1)
+print(d2)
+
+d3={"India": "Delhi", "Italy": "Rome", "Australia": "Canberra"}
+print(d3)
+
+# Keya and values both are of string type. We can also have keys and values of different data types.
+
+# Creating Empty Dictionary
+d4={}
+d5=dict()
+print(d4)
+print(d5)
+
+d6={1: "one", 2: "two", 3: "three"}
+print(d6)
+
+# To access data from the dictionary we use the keys to access their corresponding values. 
+d={100: 'abc', 200: 'def', 300: 'ghi'}
+print(d[100])
+# print(d[400])
---
+++ Basics/65_Dictionary.py
@@ -0,0 +1,30 @@
+''' 
+Dictionary:
+1. If we want to represent a group of objects as key-value pairs then we should go for dictionary.
+
+2. Python dictionary is an ordered collection of items.
+
+3. It stores elements in key/value pairs. Here, keys are unique identifiers that are associated with each value.
+Keys        Value
+India       Delhi
+Italy       Rome
+Australia   Canberra
+Keys are unique identifiers that are associated with each valye.
+
+4. dict1={1:'a', 1: 'b', 1: 'c'}    Duplicate keys are not allowed
+    dict2={1:'a', 2: 'b', 3: 'c'}    Allowed
+    
+5. Heterogenous Objects:
+dict3={1:'2.0', 'a':3, 4.5: 'b'}
+
+6. Insertion Order is not preserved.
+dict2={1:'a', 2: 'b', 3: 'c'}
+
+7. Mutable
+dict4={1: 'a', 4: 'd', 3: 'c'}
+
+8. Indexing and slicing are not allowed.
+
+9. Values in a dictionary can be of any data type and can be duplicate.
+integer, string, float and list
+'''
