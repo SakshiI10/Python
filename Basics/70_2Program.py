a52e6e6c0fcce18f1acc128fcc6849a45c5c43b3
Sakshi <iamsakshiingole@gmail.com>
2024-05-24 08:09:15 -0400

    File added

--- Basics/70_2Program.py
+++ Basics/70_2Program.py
@@ -20,4 +20,18 @@ for i in range(1, n+1):
     result_dict[i]=i*i
 print(result_dict)
 
-#
+# Creating and displaying all combinations of letters in a dictionary. 
+data={'1':['a','b'], '2':['c','d']}
+keys=list(data.keys())
+values=list(data.values())
+
+for i in range(len(values[0])):
+    for j in range(len(values[1])):
+        print(values[0][i]+values[1][j])
+        
+# Matching key values in two dictionaries
+dict1 = {'key1':1, 'key2':2, 'key3':3}
+dict2 = {'key1':1, 'key2':2}
+for key in dict1:
+    if key in dict2 and dict1[key]==dict2[key]:
+        print(f"{key}:{dict1[key]} is present in both x and y")
