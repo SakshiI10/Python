c81e45f3e0d2b57283a20f1aed65934e2eada49d
Sakshi <iamsakshiingole@gmail.com>
2024-05-30 08:22:14 -0400

    File added

---
+++ Basics/78_DateClass.py
@@ -0,0 +1,38 @@
+''' 
+Date Class
+Syntax: class datetime.date(year, month, day)
+'''
+
+''' import datetime
+my_date=datetime.date(2023,5,30)
+print("Passed date is:",my_date) '''
+
+''' 
+Functions:
+min
+max
+resolution
+year
+month
+day
+'''
+
+from datetime import date
+
+mindate=date.min 
+print("Min date supported", mindate)
+
+maxdate=date.max
+print("Max date supported", maxdate)
+
+Date=date(2024, 5, 30)
+print("Year", Date.year)
+print("Month", Date.month)
+print("Day", Date.day)
+
+today=date.today()
+print("Todays date is:",today)
+
+current_year=today.year
+print("Current year is:",current_year)
+
