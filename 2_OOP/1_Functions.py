af6ba3f898a6072c1bc57bd844102c88aed9cc59
SakshiI10 <iamsakshiingole@gmail.com>
2024-07-16 20:54:27 +0530

    File added

---
+++ 2_OOP/1_Functions.py
@@ -0,0 +1,18 @@
+'''
+Function:
+
+1. Function is like a module that divides program into smaller pieces which is a set of logically related set of statements to perform a specific task.
+
+2. Module in programming language are functions, subroutines, procedures or subproblems.
+
+3. It's a structuring element that can be defined once and used more than once, when and where required.
+
+4. When program become larger use functions, it avoids repitition of code.
+
+5. Function can pass data called parameter and also can return data as result of function.
+
+6. Functions are of 3 types:
+a. Built In Functions
+b. User Defined Functions
+c. Recursive Functions
+'''
