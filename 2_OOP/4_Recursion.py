26e2305f56ed6a21e957fd7e7ad37698a8f25bad
SakshiI10 <iamsakshiingole@gmail.com>
2024-07-16 21:12:45 +0530

    File added

--- 2_OOP/4_Recursion.py
+++ 2_OOP/4_Recursion.py
@@ -1,2 +1,15 @@
 '''
-Recursion:'''
+Recursion:
+a. Recursive function are a type of function that deals with itself. Function calls itself until some condition stops it.
+'''
+
+# Factorial Function:
+def fact(n):
+    if n==1:
+        return 1
+    else:
+        return (n*fact(n-1))
+
+num=int(input("Enter number: "))
+f=fact(num)
+print(f)
