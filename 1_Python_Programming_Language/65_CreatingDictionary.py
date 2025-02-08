c5af7a9269d55fc1f7661a729db769f3064c24cd
SakshiI10 <iamsakshiingole@gmail.com>
2025-02-08 23:06:33 +0530

    File revisited

--- 1_Python_Programming_Language/65_CreatingDictionary.py
+++ 1_Python_Programming_Language/65_CreatingDictionary.py
@@ -2,6 +2,7 @@
 Creating Dictionary:
 Dictionary keys are case sensitive, the same name but different cases of key will be treated distinctly.
 '''
+
 d1={'a': 1, 'b': 2, 'c':3}
 d2={'A': 1, 'B': 2, 'C':3}
 print(d1)
@@ -10,7 +11,7 @@ print(d2)
 d3={"India": "Delhi", "Italy": "Rome", "Australia": "Canberra"}
 print(d3)
 
-# Keya and values both are of string type. We can also have keys and values of different data types.
+# Keys and values both are of string type. We can also have keys and values of different data types.
 
 # Creating Empty Dictionary
 d4={}
@@ -24,4 +25,3 @@ print(d6)
 # To access data from the dictionary we use the keys to access their corresponding values. 
 d={100: 'abc', 200: 'def', 300: 'ghi'}
 print(d[100])
-# print(d[400])
