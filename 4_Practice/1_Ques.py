9202a3fd13864bb2d5eb108735e95afc459036c7
SakshiI10 <iamsakshiingole@gmail.com>
2025-03-25 16:34:13 +0530

    folder added

--- 4_Practice/1_Ques.py
+++
@@ -1,8 +0,0 @@
-# Concept 1: Lists inside a tuple can be edited but tuple can't.
-
-siblings_ids = (1, 2, [3, 4], 11, 9)
-
-siblings_ids[2].append(5)  # Modifies the list at index 2
-print(siblings_ids)  # Output: (1, 2, [3, 4, 5], 11, 9)
-siblings_ids[4] = 10  # Modifies the list at index 2
-print(siblings_ids)  # Output: (1, 2, [3, 4, 5], 11, 9)
--- 4_Practice/2_Ques.py
+++
@@ -1,7 +0,0 @@
-# Concept 2: DIctionary don't have a concecpt of indexing, the values are changed using keys.
-
-c = {}
-c[1] = 1
-c['1'] = 2
-c[1] += 1
-print(c)
