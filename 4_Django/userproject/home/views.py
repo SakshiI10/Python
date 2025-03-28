7b47ffd810539d3031c57587d097862b670f1173
SakshiI10 <iamsakshiingole@gmail.com>
2025-03-28 19:42:28 +0530

    Final push

--- 4_Django/userproject/home/views.py
+++ 4_Django/userproject/home/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+def index(request):
+    if request.user.is_anonymous:
+        return redirect("/login") 
+    return render(request, 'index.html')
+
+def loginUser(request): 
+    if request.method == "POST":  
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user) 
+            return redirect("/")
+        else:
+            messages.error(request, "Invalid username or password")  
+
+    return render(request, 'login.html') 
+
+def logoutUser(request):
+    logout(request)
+    return redirect("/login")
