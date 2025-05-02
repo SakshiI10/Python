5db0e2e41e420982c8f4335b2c71b0d6778ec10e
SakshiI10 <iamsakshiingole@gmail.com>
2025-05-02 12:23:17 +0530

    file edited

--- 2_OOP/15_Polymorphism.py
+++ 2_OOP/15_Polymorphism.py
@@ -3,20 +3,23 @@ Polymorphism:
 1. This is the concept of using common operation in difference ways for difference data input.
 
 2. When function with same name are used for difference data types they are called polymorphic functions.
-'''
-#3. Types of Polymorphism:
-#a. Compile time Polymorphism:
 
+3. Types of Polymorphism:
+a. Compile time Polymorphism
+b. Run time Polymorphism 
+'''
+
+#a. Compile time Polymorphism:
 #i. Operator Overloading: Operator overloading allows the same operator to have different meanings based on the context. In Python, this is done by defining special methods in a class, such as __add__ for the + operator.
 class Rectangle:
     def __init__(self, width, height):
         self.width = width  # Initialize the width attribute
         self.height = height  # Initialize the height attribute
 
+    # Overloading the + Operator
     def __add__(self, other):
-        # Return a new Rectangle object with the sum of the widths and heights
-        return Rectangle(self.width + other.width, self.height + other.height)
-
+         return Rectangle(self.width + other.width, self.height + other.height)
+    
     def __str__(self):
         # Provide a string representation of the Rectangle object
         return f"Operator Overloading: Rectangle(width={self.width}, height={self.height})"
@@ -37,13 +40,16 @@ print(rect3)  # Output: Rectangle(width=8, height=10)
 # ii. Method Overloading: Method overloading allows multiple methods in a class to have the same name but different parameters (different type or number of parameters). 
 class MathOperations:
     # Define the add method with method overloading capability using default arguments
-    def add(self, a, b, c=0):
-        return a + b + c
+    def add(self, *args):
+        total = 0
+        for num in args:
+            total += int(num)  # convert to int
+        return total
 
 # Create an instance of MathOperations
 math_ops = MathOperations()
 print("Method Overloading:", math_ops.add(1, 2))
-print("Method Overloading:", math_ops.add(1, 2, 3))
+print("Method Overloading:", math_ops.add(1, 2, '3', 4))
 
 
 
@@ -54,12 +60,12 @@ print("Method Overloading:", math_ops.add(1, 2, 3))
 # Define the Bird class with a method `fly`
 class Bird:
     def fly(self):
-        return "Flying" 
+        return "Bird is flying in the sky" 
 
 # Define the Airplane class with a method `fly`
 class Airplane:
     def fly(self):
-        return "Flying in the sky" 
+        return "Aeroplane is in the sky" 
 
 # Define a function that takes any object with a `fly` method
 def let_it_fly(entity):
@@ -90,5 +96,5 @@ class derived(middle):
     def method(self):
         print("I'm in derived")
         super(derived, self).method()
-obj=derived()
-obj.method()
+d=derived()
+d.method()
