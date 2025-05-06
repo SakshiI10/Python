4ef4e58c0fb1a973bf8a0417ffff1783d45dbe7a
SakshiI10 <iamsakshiingole@gmail.com>
2025-05-06 20:08:58 +0530

    file edited

--- 2_OOP/19_Super.py
+++ 2_OOP/19_Super.py
@@ -4,3 +4,18 @@ a) super() follows a specific path (called Method Resolution Order or MRO).
 b) It calls only one parent at a time, not both.
 c) If your parent classes are (base1, base2) don’t use super, because parts of the chain can skip. So, if you just use super() in the child class, only one base class might get initialized, and the other will be ignored.
 '''
+
+class A:
+    def msg(self):
+        print("A")
+
+class B:
+    def msg(self):
+        print("B")
+
+class C(A, B):
+    pass
+
+c = C()
+c.msg()
+# Python uses left-to-right depth-first search for method resolution. That's why A printed.
--- 2_OOP/22_Prac.py
+++ 2_OOP/22_Prac.py
@@ -1,13 +1,1 @@
-class A:
-    def msg(self):
-        print("A")
 
-class B:
-    def msg(self):
-        print("B")
-
-class C(A, B):
-    pass
-
-c = C()
-c.msg()
